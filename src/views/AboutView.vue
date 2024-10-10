d9373b114a7f3e14b89221c093cf04c0f2fbea5d
JulioCamposR <julio.camposr95@gmail.com>
2024-10-10 00:05:48 -0300

    agregar

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>About Us</h1>
+      <p>Information about the app goes here.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutView'
+  }
+  </script>
+  
---
+++ src/views/ContactView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Contact Us</h1>
+      <p>Contact information goes here.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ContactView'
+  }
+  </script>
+  
